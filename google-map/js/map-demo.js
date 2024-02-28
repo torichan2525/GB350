5700887f30cf2180d2fdd8ea1d376f3318dedbe4
torichan2525 <104830675+torichan2525@users.noreply.github.com>
2024-02-28 09:33:13 +0900

    Add files via upload

---
+++ google-map/js/map-demo.js
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+async function initMap() {
+  const contentString = `
+    <div>
+      <h1>タイトル</h1>
+      <div>
+        <p>
+        ここに詳細を記載
+        </p>
+        <p>
+          Attribution: Uluru,
+          <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">
+            https://en.wikipedia.org/w/index.php?title=Uluru
+          </a>
+          (last visited June 22, 2009).
+        </p>
+      </div>
+    </div>`;
+  const infoWindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Uluru",
+  });
+
+  const marker = document.querySelector('gmp-advanced-marker');
+  marker.addEventListener('gmp-click', () => {
+    infoWindow.open({anchor: marker});
+  });
+}
+
--- google-map/js/map.js
+++ google-map/js/map.js
@@ -1,58 +1,113 @@
-function map_canvas() {
+type='text/javascript' src='https://maps.google.com/maps/api/js?key=AIzaSyCZtuhSviWhr9rnUzDviTrXiZk1GHvNDEc'
+
+	() => {
     //マーカーの情報
-      var data = new Array();
-        data.push({
-          lat: '43.064323', //緯度
-          lng: '141.346878', //経度
-          url: 'https://gb350.com/hokkaido/' //リンク先
-        });
-        data.push({
-          lat: '35.678187',
-          lng: '139.76817',
-          url: 'http://www.yaesu-book.co.jp/'
-      });
-        data.push({
-          lat: '35.682063',
-          lng: '139.773448',
-          url: 'http://www.nihonbashi-tokyo.jp/'
-      });
-      
+    var data = new Array();
+    data.push({
+      lat: '38.341656',
+      lng: '137.416992',
+      url: 'http://www.yaesu-book.co.jp/'
+    });
+    data.push({
+      lat: '43.064323', //緯度
+      lng: '141.346878', //経度
+      url: 'https://gb350.com/hokkaido/' //リンク先
+    });
+    data.push({
+      lat: '35.465778',
+      lng: '139.622299',
+      url: 'http://www.yaesu-book.co.jp/'
+    });
+
+    //data.push({
+    // lat: '35.682063',
+    // lng: '139.773448',
+    // url: 'http://www.nihonbashi-tokyo.jp/'
+    //});
     //初期位置に、上記配列の一番初めの緯度経度を格納
-      var latlng = new google.maps.LatLng(data[0].lat, data[0].lng);
-      
-      var opts = {
-        zoom: 15,//地図の縮尺
-        center: latlng, //初期位置の変数
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-      
+    var latlng = new google.maps.LatLng(data[0].lat, data[0].lng);
+
+    var opts = {
+      zoom: 4, //地図の縮尺
+      center: latlng, //初期位置の変数
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
     //地図を表示させるエリアのidを指定
-      var map = new google.maps.Map(document.getElementById("map"), opts);
-      
+    var map = new google.maps.Map(document.getElementById("map"), opts);
+
     //マーカーを配置するループ
-      for (i = 0; i < data.length; i++) {
-        var markers = new google.maps.Marker({
-          position: new google.maps.LatLng(data[i].lat, data[i].lng),
-          map: map
-        });
-        //クリックしたら指定したurlに遷移するイベント
-        google.maps.event.addListener(markers, 'click', (function(url){
-          return function(){ location.href = url; };
-        })(data[i].url));
-      }
+    for (i = 0; i < data.length; i++) {
+      var markers = new google.maps.Marker({
+        position: new google.maps.LatLng(data[i].lat, data[i].lng),
+        map: map
+      });
+      //クリックしたら指定したurlに遷移するイベント
+      google.maps.event.addListener(markers, 'click', (function (url) {
+        return function () { location.href = url; };
+      })(data[i].url));
     }
+  }
       
     //地図描画を実行
     google.maps.event.addDomListener(window, 'load', map_canvas);
-
-<script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyCZtuhSviWhr9rnUzDviTrXiZk1GHvNDEc'></script>
-
-<script>
-  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "AIzaSyCZtuhSviWhr9rnUzDviTrXiZk1GHvNDEc",
-    // Add other bootstrap parameters as needed, using camel case.
-    // Use the 'v' parameter to indicate the version to load (alpha, beta, weekly, etc.)
-  });
-</script>
-
     
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  
+      ga('create', 'UA-47542229-1', 'auto');
+      ga('send', 'pageview');
+
+
+
+
+
+
+      /**
+ * @license
+ * Copyright 2023 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+async function initMap() {
+  const contentString = `
+    <div>
+      <h1>Uluru</h1>
+      <div>
+        <p>
+          <b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large
+          sandstone rock formation in the southern part of the
+          Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi)
+          south west of the nearest large town, Alice Springs; 450&#160;km
+          (280&#160;mi) by road. Kata Tjuta and Uluru are the two major
+          features of the Uluru - Kata Tjuta National Park. Uluru is
+          sacred to the Pitjantjatjara and Yankunytjatjara, the
+          Aboriginal people of the area. It has many springs, waterholes,
+          rock caves and ancient paintings. Uluru is listed as a World
+          Heritage Site.
+        </p>
+        <p>
+          Attribution: Uluru,
+          <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">
+            https://en.wikipedia.org/w/index.php?title=Uluru
+          </a>
+          (last visited June 22, 2009).
+        </p>
+      </div>
+    </div>`;
+  const infoWindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Uluru",
+  });
+
+  const marker = document.querySelector('gmp-advanced-marker');
+  marker.addEventListener('gmp-click', () => {
+    infoWindow.open({anchor: marker});
+  });
+}
+
+window.initMap = initMap;
